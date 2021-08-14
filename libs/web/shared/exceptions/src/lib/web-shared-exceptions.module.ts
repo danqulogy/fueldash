a216c3320a9a39ec67d2aa4fd0b7e382a04c14de
Bernard White <white@fulleffect.studio>
2021-08-14 01:55:43 +0100

    added exceptions module

--- libs/web/shared/exceptions/src/lib/web-shared-exceptions.module.ts
+++ libs/web/shared/exceptions/src/lib/web-shared-exceptions.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FullscreenComponent } from '@metrotenants/web/shared/layouts';
+
+const routes:Routes = [
+  {
+    path: 'exceptions',
+    component: FullscreenComponent,
+    children: [
+      {
+        path: '404',
+        component: NotFoundComponent,
+        data: {title: 'Not Found'}
+      }
+
+    ]
+  }
+]
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [
+    NotFoundComponent
+  ],
 })
 export class WebSharedExceptionsModule {}
